9e97aad93eeb4347b31422674b2d3039088f7ee1
srijyothsna5354 <srijyothsna5354@gmail.com>
2023-09-26 19:17:19 +0200

    updated .gitignore

--- server/env/environment.js
+++
@@ -1,9 +0,0 @@
-const cosmosPort = 10255; // replace with your port
-const dbName = 'travelblog';
-const key = 'sCliM0Z57LtUA8K83FjMSy9cKSsyO2vm9INCrZMwsian9VmadcepSHdrQ3CpsZMOChdLM4CVFhyJACDbifiW4Q==';
-
-module.exports = {
-  cosmosPort,
-  dbName,
-  key
-};
